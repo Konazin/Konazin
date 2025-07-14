3e4c3f70ab217534a8c0c7dd085c5461b917bf53
Miguel/Kona <162783837+Konazin@users.noreply.github.com>
2025-07-14 18:43:52 -0300

    Add files via upload

---
+++ organizador.py
@@ -0,0 +1,15 @@
+import os
+import shutil
+
+camin = str(input('adicione o caminho da pasta corretamente'))
+faz = str(input('o que quer fazer? [copiar][apagar]'))
+
+def copia():
+    t
+
+
+
+if faz == 'copiar':
+    copia()
+elif faz == 'apagar':
+    apaga()
