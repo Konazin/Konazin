4df21a7b1f8ee4a4246e44900c9c649f96cb05ad
Miguel/Kona <162783837+Konazin@users.noreply.github.com>
2025-07-16 14:09:51 -0300

    Add files via upload

--- organizador.py
+++ organizador.py
@@ -3,12 +3,20 @@ import shutil
 
 camin = str(input('adicione o caminho da pasta corretamente'))
 faz = str(input('o que quer fazer? [copiar][apagar]'))
+arquivo = 
 
 def copia():
-    t
+    shutil.copy(dst=camin, )
+    print('arquivo copiado')
 
+def copiadir():
+    try:
+         shutil.copy(arquivo, )
+    except shutil.SameFileError:
 
-
+def apagar():
+    os.remove(camin)
+    
 if faz == 'copiar':
     copia()
 elif faz == 'apagar':
