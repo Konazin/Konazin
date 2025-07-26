8d337da925615246d7878d68ee75b358343fb0d6
Konazin <m4caun4@gmail.com>
2025-07-25 22:09:22 -0300

    modified:   bruteforcezip.py Funcional

--- bruteforcezip.py
+++ bruteforcezip.py
@@ -1,26 +1,22 @@
-from zipfile import *
 import string
-import random
+import pyzipper
+from itertools import product
 
-arq = str(input('adicione o caminho do zip'))
+arq = str(input('adicione o caminho do zip:'))
 force = []
-senha = string.ascii_uppercase + string.ascii_lowercase + string.punctuation
+senha = string.digits
 
-def brute():
-    for _ in range(6):
-            cho = random.randint(1,2)
-            if cho == 1:
-                  force = random.choice(senha)
-            else:
-                  random.randint(0,9)
-with ZipFile(arq, 'r') as zpref:
-    pause = False
-    while pause == False:
-        brute()
-        bt = ''.join(force)
-        def ext():
-             zpref.extractall(path=arq, pwd=bt)
-             return True
-        if ext == True:
-             pause = True
-print('Pasta violada')
+def ext(sen):
+    try:
+        with pyzipper.AESZipFile(arq, 'r') as zp:
+            zp.extractall(pwd=sen.encode())
+        return True
+    except:
+        return False
+
+for comb in product(senha, repeat=3):
+    res = ''.join(comb)
+    print(f'Testando: {res}')
+    if ext(res):
+        print(f'violado com: {res}')
+        break
