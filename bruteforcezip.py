b00efa01d6ec3267125dfa09858fa058e0dff7ae
Konazin <m4caun4@gmail.com>
2025-07-26 15:30:11 -0300

    opções de escolhas maiores

--- bruteforcezip.py
+++ bruteforcezip.py
@@ -2,9 +2,29 @@ import string
 import pyzipper
 from itertools import product
 
+esc1 = str((input('você quer que seja incluida letras?[y][n] ')))
+esc2 = str(input('você quer que seja incluida numeros? [y][n] '))
+esc3 = str(input('você quer que seja incluido caracteres especiais?[y][n] '))
 cs = int(input('quantos digitos tem(ou você acredita que tenha) a senha?: '))
 arq = str(input('adicione o caminho do zip: '))
-senha = string.digits + string.ascii_lowercase + string.ascii_uppercase
+
+if esc1 and esc2 and esc3 == 'y':
+    senha = string.digits + string.ascii_lowercase + string.ascii_uppercase + string.punctuation
+elif esc2 and esc3 == 'y':
+    senha = string.digits + string.punctuation
+elif esc1 and esc3 == 'y':
+    senha = string.ascii_lowercase + string.ascii_uppercase + string.punctuation
+elif esc1 and esc2 == 'y':
+    senha = string.digits + string.ascii_lowercase + string.ascii_uppercase
+elif esc1 == 'y':
+    senha = string.ascii_lowercase + string.ascii_uppercase
+elif esc2 == 'y':
+    senha = string.digits
+elif esc3 == 'y':
+    senha = string.punctuation
+else:
+    print('não foi uma escolha válida')
+    exit()
 
 def ext(sen):
     try:
