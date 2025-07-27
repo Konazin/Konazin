d22b7a8dd49ec5b16ba53ae33a95f3e6be0f4487
Konazin <m4caun4@gmail.com>
2025-07-27 16:58:13 -0300

    multiprocess added

--- bruteforcezip.py
+++ bruteforcezip.py
@@ -1,42 +1,42 @@
 import string
 import pyzipper
 from itertools import product
+from multiprocessing import Pool, cpu_count
 
-esc1 = str((input('você quer que seja incluida letras?[y][n] ')))
-esc2 = str(input('você quer que seja incluida numeros? [y][n] '))
-esc3 = str(input('você quer que seja incluido caracteres especiais?[y][n] '))
-cs = int(input('quantos digitos tem(ou você acredita que tenha) a senha?: '))
+esc1 = str((input('incluir letras?[y][n] ')))
+esc2 = str(input('incluir numeros? [y][n] '))
+esc3 = str(input('incluir caracteres especiais?[y][n] '))
+cs = int(input('tamanho estimado da senha?: '))
 arq = str(input('adicione o caminho do zip: '))
 
-if esc1 and esc2 and esc3 == 'y':
-    senha = string.digits + string.ascii_lowercase + string.ascii_uppercase + string.punctuation
-elif esc2 and esc3 == 'y':
-    senha = string.digits + string.punctuation
-elif esc1 and esc3 == 'y':
-    senha = string.ascii_lowercase + string.ascii_uppercase + string.punctuation
-elif esc1 and esc2 == 'y':
-    senha = string.digits + string.ascii_lowercase + string.ascii_uppercase
-elif esc1 == 'y':
-    senha = string.ascii_lowercase + string.ascii_uppercase
-elif esc2 == 'y':
-    senha = string.digits
-elif esc3 == 'y':
-    senha = string.punctuation
-else:
-    print('não foi uma escolha válida')
+senha = ''
+if esc1 == 'y':
+    senha += string.ascii_letters
+if esc2 == 'y':
+    senha += string.digits
+if esc3 == 'y':
+    senha += string.punctuation
+if not senha:
+    print('sem seleção, encerrando...')
     exit()
 
 def ext(sen):
     try:
         with pyzipper.AESZipFile(arq, 'r') as zp:
             zp.extractall(pwd=sen.encode())
-        return True
+        return (sen, True)
     except:
-        return False
+        return (sen, False)
 
-for comb in product(senha, repeat= cs):
-    res = ''.join(comb)
-    print(f'Testando: {res}')
-    if ext(res):
-        print(f'violado com: {res}')
-        break
+def res():
+    for sla in product(senha, repeat=cs):
+        yield ''.join(sla)
+
+if __name__ == "__main__":
+    with Pool(cpu_count()) as pool:
+        for comb, sus in pool.imap_unordered(ext, res(), chunksize=500):
+            print(f'Testando: {comb}')
+            if sus:
+                print(f'violado com: {comb}')
+                pool.terminate()
+                break
