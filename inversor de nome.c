c39c65590b8e4226970116ab8ec62afc4f64581f
Konazin <m4caun4@gmail.com>
2025-07-30 23:32:04 -0300

    modified:   C 	renamed:    C.c -> inversor de nome.c

--- inversor de nome.c
+++ inversor de nome.c
@@ -5,10 +5,7 @@ int main(){
     char nome[30];
     printf("bote o nome que pretende reverter: \n");
     scanf("%29[^\n]s", nome);
-    fflush(stdin);
     for (i=29; i!=-1; i--){
-        for (j=0 ;j!=30; j++);
-            nome[j] = nome[i];
+        printf("%c", nome[i]);
     }
-    printf("%s", nome);
 }
