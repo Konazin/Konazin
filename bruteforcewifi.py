e881a02478bce5ac7540726ff4d18f299aee25c8
konazin <m4caun4@gmail.com>
2025-08-31 15:55:21 -0300

    renamed:    bruteforcewifi -> bruteforcewifi.py

--- bruteforcewifi.py
+++ bruteforcewifi.py
@@ -1,6 +1,11 @@
 import random
-from itertools import product
+import scapy
+from scapy.all import *
 from multiprocessing import pool, cpu_count
 
 wifi = str(input("adicione o ip da rede: "))
 dist = int(input("adicione a quantidade de digitos da senha: "))
+mac = str(input("digite o endereço mac"))
+
+def cheiro():
+    
